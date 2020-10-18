b8ef6acf0f1c675aeebb1e6234d3996c1518b784
Callum <callumec>
2020-10-18 17:11:53 +0100

    Altered to allow user input of PID with conversion to hex.

--- open_proc.py
+++ open_proc.py
@@ -1,21 +1,20 @@
 import ctypes
 
 k_handle = ctypes.WinDLL("Kernel32.dll")
-
 PROCESS_ALL_ACCESS = (0x000F0000 | 0x00100000 | 0xFFF)
 dwDesiredAccess = PROCESS_ALL_ACCESS
 bInheritHandle = False
-dwProcessID = 0x2DC8
+
+intProcessID = int(input("Enter a PID: "))
+dwProcessID = int(hex(intProcessID), base=16)
 
 response = k_handle.OpenProcess(dwDesiredAccess, bInheritHandle, dwProcessID)
 
-error = k_handle.GetLastError()
-
-if (error != 0):
-    print("Error: {0}".format(error))
-    exit(1)
-
 if (response <= 0):
     print("Handle was not created.")
+    error = k_handle.GetLastError()
+    if (error != 0):
+        print("Error: {0}".format(error))
+        exit(1)
 else:
     print("Handle created!")
